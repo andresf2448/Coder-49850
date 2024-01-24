490de3840c1c4c39179ece17dbfa2d8e44ce0e62
andresf2448 <andresf2448@gmail.com>
2024-01-24 08:34:09 -0500

    Clase 10

---
+++ Clase 10/main.js
@@ -0,0 +1,29 @@
+//EVENTOS
+
+// let boton = document.getElementById("boton");
+
+// const respuesta = () => {
+//   alert("Hola a todos");
+// };
+
+//1 forma
+//OBSERVACION: debemos pasar la declaración de la función no su ejecución
+// boton.addEventListener("click", respuesta);
+
+//2 forma
+//OBSERVACION: debemos pasar la declaración de la función no su ejecución
+// boton.onclick = respuesta;
+
+// let evento = prompt("Ingrese el evento requerido");
+
+// boton.addEventListener(evento, respuesta);
+
+// const respuesta = (nombre) => {
+//   alert(`Hola ${nombre}`);
+// };
+
+// let nombre = prompt("Ingrese el nombre de la persona");
+
+//para pasar parametros a la funcion podemos pasarlos como normalmente lo hacemos
+//pero le agregamos () => la arrow function
+// boton.addEventListener("click", () => respuesta(nombre));
