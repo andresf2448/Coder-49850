271bc0efbf1924548795d43945599ff3aaebeb90
andresf2448 <andresf2448@gmail.com>
2024-02-07 09:42:51 -0500

    Clase 14

--- Clase 14/main.js
+++ Clase 14/main.js
@@ -60,18 +60,98 @@ let boton = document.getElementById("boton");
 //   });
 // });
 
-boton.addEventListener("click", () => {
-  Swal.fire({
-    title: "Ingrese le nombre del producto a buscar",
-    input: "text",
-    showCancelButton: true,
-    confirmButtonText: "Look up",
-  }).then((result) => {
-    // const producto = productos.find((item) => item.nombre === result.value);
-    if (result.isConfirmed) {
-      Swal.fire({
-        title: result.value,
-      });
-    }
-  });
-});
+// boton.addEventListener("click", () => {
+//   Swal.fire({
+//     title: "Ingrese le nombre del producto a buscar",
+//     input: "text",
+//     showCancelButton: true,
+//     confirmButtonText: "Look up",
+//   }).then((result) => {
+//     // const producto = productos.find((item) => item.nombre === result.value);
+//     if (result.isConfirmed) {
+//       Swal.fire({
+//         title: result.value,
+//       });
+//     }
+//   });
+// });
+
+//TOASTIFY
+// boton.addEventListener("click", () => {
+//   Toastify({
+//     text: "Probando toast!",
+//     duration: 3000,
+//   }).showToast();
+// });
+
+// boton.addEventListener("click", () => {
+//   Toastify({
+//     text: "Probando toast!",
+//     duration: 3000,
+//     style: {
+//       background: "linear-gradient(to left, #00b09b, #96c92d)",
+//     },
+//   }).showToast();
+// });
+
+// boton.addEventListener("click", () => {
+//   Toastify({
+//     text: "Probando toast!",
+//     duration: 3000,
+//     onClick: () => {
+//       Toastify({
+//         text: "Clickeaste un Toast!",
+//         duration: 1500,
+//       }).showToast();
+//     },
+//   }).showToast();
+// });
+
+// boton.addEventListener("click", () => {
+//   Toastify({
+//     text: "Click aquí para ir a Coder!",
+//     duration: 3000,
+//     destination: "https://www.coderhouse.com",
+//   }).showToast();
+// });
+
+//LUXON
+// const DateTime = luxon.DateTime;
+
+// const now = DateTime.local(2024, 2, 7, 9);
+// console.log(now.toString());
+
+// const now = DateTime.now();
+// console.log(now.toString());
+// console.log(now.year);
+// console.log(now.month);
+// console.log(now.day);
+// console.log(now.second);
+// console.log(now.weekday);
+// console.log(now.zoneName);
+// console.log(now.daysInMonth);
+// console.log(now.toLocaleString());
+// console.log(now.toLocaleString(DateTime.DATE_FULL));
+// console.log(now.toLocaleString(DateTime.TIME_SIMPLE));
+
+// console.log(now.setLocale('en').toLocaleString(DateTime.DATE_FULL));
+// console.log(now.setLocale('es').toLocaleString(DateTime.DATE_FULL));
+
+// console.log(now.toLocaleString(DateTime.DATETIME_SHORT));
+
+// const suma = now.plus({ hours: 1, minutes: 10 });
+// console.log(suma.toLocaleString(DateTime.DATETIME_SHORT));
+
+// const resta = now.minus({month: 1, days: 1});
+// console.log(resta.toLocaleString(DateTime.DATETIME_SHORT));
+
+const DateTime = luxon.DateTime;
+const Interval = luxon.Interval;
+
+const now = DateTime.now();
+const navidad = DateTime.local(2024, 12, 24);
+const i = Interval.fromDateTimes(now, navidad);
+
+console.log(i.length('days'));
+console.log(i.length("hours"));
+console.log(i.length("minutes"));
